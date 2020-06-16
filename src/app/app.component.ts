5925dcf09b219a15ae0c7e06d9fad0a8d2fdfa1c
nardos-negussie <nardosnegussie519461@gmail.com>
2020-06-16 06:43:30 +0300

    Revert "chapter 1 - angular reddit"
    This reverts commit 1bfa082de7aae63307cbdd7e5b68340f2b5d98eb.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { Article } from './article/article.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,24 +7,4 @@ import { Article } from './article/article.model';
 })
 export class AppComponent {
   title = 'ngBible';
-  articles: Article[];
-
-  constructor() {
-    this.articles = [];
-  }
-
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    if (title.value && link.value) {
-      this.articles.push(new Article(title.value, link.value));
-      title.value = '';
-      link.value = '';
-    }
-    return false;
-  }
-
-  sortedArticles(): Article[] {
-    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
-  }
-
 }
-
